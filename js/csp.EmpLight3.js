1106887dfb1606cf98070d925ae5cca7a0ad0d6b
doublefint <doublefint@gmail.com>
2011-08-04 17:34:19 +0000

    Восстановлена функциональность клиентского javascript

--- js/csp.EmpLight3.js
+++ js/csp.EmpLight3.js
@@ -1,67 +1,69 @@
-
 var InnerSplitter="~";
-var sessionid="#(%session.SessionId)#";
-var kontr='#($G(kontr))#';
-var instrtype='#($G(instrtype))#';
 var d1=new Date();
-
-// функция вызываемся автоматически послезагрузки документа
-function load(){
-var kontrArray=kontr.split("~");
-var instrtypeArray=instrtype.split("~");
-if((kontrArray[0]!="all")&(kontrArray[0]!="")){SetSelectData("ChooseKontragent",kontr)}
-if((instrtypeArray[0]!="all")&(instrtypeArray[0]!="")){SetSelectData("ChooseInstrType",instrtype)}
-var d2=new Date();
-
-}
+function load(){ // функция вызываемся автоматически послезагрузки документа
+	var kontrArray=kontr.split("~"), instrtypeArray=instrtype.split("~");
+	if((kontrArray[0]!="all")&(kontrArray[0]!="")){
+		SetSelectData("ChooseKontragent",kontr);
+	}
+	if((instrtypeArray[0]!="all")&(instrtypeArray[0]!="")){
+		SetSelectData("ChooseInstrType",instrtype);
+	}
+	var d2=new Date();
+};
 
 // печать документа созданного инструкцией
 function prnt(idclass){
-var docid=idclass.split("@")[0]
-var docclass=idclass.split("@")[1]
-window.open("print.csp?id="+docid+"&class="+docclass+"&sessionid="+sessionid)
- }
+	var docid=idclass.split("@")[0], docclass=idclass.split("@")[1]
+	window.open("print.csp?id="+docid+"&class="+docclass+"&sessionid="+sessionid)
+};
  
 //изменить состояние инструкции
 function chstat(docid,innerdoc){
- var newstat=window.showModalDialog("instrstates.csp","","center:yes;status:no;dialogHeight:300px;dialogWidth:300px;resizable:yes;")
- if(newstat!=null){
-	 var ok=#server(Operation.InstrActions.changestat(docid,newstat))#
-	 if(ok.split("@")[0]!=1){alert(ok)}
-	 else{
-		 document.getElementById("Instr"+docid+"Stat").innerText=ok.split("@")[1];
+ 	var newstat=window.showModalDialog("instrstates.csp","","center:yes;status:no;dialogHeight:300px;dialogWidth:300px;resizable:yes;")
+	if(newstat!=null){
+		 var ok=server.changeState(docid,newstat);
+		 if(ok.split("@")[0]!=1){
+			 alert(ok)
+		 } else {
+			 document.getElementById("Instr"+docid+"Stat").innerText=ok.split("@")[1];
 		 }
-	 //location.href="emplight.csp?instr="+docid+"&newstat="+newstat;
+		 //location.href="emplight.csp?instr="+docid+"&newstat="+newstat;
 	 }
  }
  
  //открыть внутреннюю инструкцию
 function lookinnerinstr(idclass){
-var docclass=idclass.split("@")[1]
-var docid=idclass.split("@")[0]
-var ok=window.showModalDialog("InnerInstructNew.csp?DocId="+docid+"&ClassName="+docclass+"&sessionid="+sessionid,"","center:yes;status:no;dialogHeight:600px;dialogWidth:600px;resizable:yes;");
-#server(Store.Action.KillTempAddress())#
+	var docclass=idclass.split("@")[1]
+	var docid=idclass.split("@")[0]
+	var ok=window.showModalDialog("InnerInstructNew.csp?DocId="+docid+"&ClassName="+docclass+"&sessionid="+sessionid,"","center:yes;status:no;dialogHeight:600px;dialogWidth:600px;resizable:yes;");
+	server.killTempAddress();
 }
 
 // создать внутреннюю инструкцию
 function create(cl,docid){
 	//создать дочерний документ основываясь на текущем
-	var ok=#server(Docs.Action.CreateChildDoc(docid,cl))#
-	if(!isNaN(ok)){window.location.reload();}
-	else{alert(ok)}
+	var ok=server.createChildDoc(docid,cl)
+	if(!isNaN(ok)){
+		window.location.reload();
+	} else{
+		alert(ok)
+	}
 }
 
 // указать реквизиты почтового документа
 function post(docid){
-var ok=window.showModalDialog("DictItem.csp?skip=InstDate,Name,Comment,Depot1,State,Oper,Operation,innerinstr,dostavka,imp,showcolor,Kontr,Dat,Tim,User1,Summa,Stat,Source,CloseDate,CloseTime,CloseUser&show=postnumber,postdata",docid+";Operation.Instructions","cener:yes;status:no;dialogHeight:350px;dialogWidth:400px;resizable:yes;")
+	var ok=window.showModalDialog("DictItem.csp?skip=InstDate,Name,Comment,Depot1,State,Oper,Operation,innerinstr,dostavka,imp,showcolor,Kontr,Dat,Tim,User1,Summa,Stat,Source,CloseDate,CloseTime,CloseUser&show=postnumber,postdata",docid+";Operation.Instructions","cener:yes;status:no;dialogHeight:350px;dialogWidth:400px;resizable:yes;")
 }
 
 // обработать внутреннюю инструкцию
 function savestore(InstrID,NameInner){
 if(window.confirm("Занести данные документа "+NameInner+" в остатки?")){
-ok=#server(Store.Action.SaveQuantFromInstr(InstrID))#
-if(isNaN(ok)){alert(ok)}
-else{alert("Запись об остатках товаров успешно занесена в базу.");}
+	ok=server.saveQuantFromInstr(InstrID);
+	if(isNaN(ok)){
+		alert(ok)
+	} else {
+		alert("Запись об остатках товаров успешно занесена в базу.");
+	}
 }
 }
 
@@ -69,59 +71,60 @@ function ShowKontr(KontrID){
 	ok=window.showModalDialog("kontragent.csp?KontrId="+KontrID,"","center:yes;status:no;dialogHeight:600px;dialogWidth:800px;resizable:yes;")
 }
 
-function SetSelectData(SelectObjID,newData){
-	if($g(SelectObjID).options.length==2){
-		$g(SelectObjID).options.length=3
+///Установить выбранное значение в списке
+function SetSelectData(selID,newData){
+	var data=newData.split("~"), id=data[0], value=data[1];
+	var sel=$g(selID), len=sel.options.length, find=null;
+	for (var i=0;i<len;i++){
+	 var opt=sel.options[i]; (opt.value!==id)?opt.removeAttribute("selected"):find=i
 	}
-	$g(SelectObjID).options(2).value=newData.split("~")[0];
-	$g(SelectObjID).options(2).text=newData.split("~")[1];
-	$g(SelectObjID).selectedIndex=2;
+	if (!find) { //Справочник контагентов?
+		sel.options.length+=1; find=len; var opt=sel.options[len]; opt.value=id; opt.text=value;
+	}
+	if (find) sel.options[find].setAttribute("selected","selected");
 }
 
 // Выбрать контрагента
 function ChooseKontragentFunc(){
-var UsersSelection=ChooseKontragent.options(ChooseKontragent.selectedIndex).value;
-if(UsersSelection=="choose"){
-	var newKontragent=window.showModalDialog("Dictionary.csp","Common.Kontragent","center:yes;status:no;dialogHeight:400px;dialogWidth:650px;resizable:yes;");
-	if(newKontragent==null){
-		ChooseKontragent.selectedIndex=0;
-		return false;}
-	SetSelectData("ChooseKontragent",newKontragent);
+	var UsersSelection=ChooseKontragent.options(ChooseKontragent.selectedIndex).value;
+	if(UsersSelection=="choose"){
+		var newKontragent=window.showModalDialog("Dictionary.csp","Common.Kontragent","center:yes;status:no;dialogHeight:400px;dialogWidth:650px;resizable:yes;");
+		if(newKontragent==null){
+			ChooseKontragent.selectedIndex=0;
+			return false;
+		}
+		SetSelectData("ChooseKontragent",newKontragent);
+	}
 	ReloadWithParams();
-}
-if(UsersSelection=="all"){
-	ReloadWithParams();
-}
+	
+
 }
 
 function ReloadWithParams(){
-	var AdditionalParams="";
-	var AdditionalParams=AdditionalParams+"kontr="+ChooseKontragent.options(ChooseKontragent.selectedIndex).value+"~"+ChooseKontragent.options(ChooseKontragent.selectedIndex).text
-	var AdditionalParams=AdditionalParams+"&instrtype="+ChooseInstrType.options(ChooseInstrType.selectedIndex).value+"~"+ChooseInstrType.options(ChooseInstrType.selectedIndex).text
-	window.location.href="emplight.csp?"+AdditionalParams;
+	var AdditionalParams="kontr="+ChooseKontragent.options(ChooseKontragent.selectedIndex).value+"~"+ChooseKontragent.options(ChooseKontragent.selectedIndex).text
+	 					 +"&instrtype="+ChooseInstrType.options(ChooseInstrType.selectedIndex).value+"~"+ChooseInstrType.options(ChooseInstrType.selectedIndex).text
+	window.location.href="?"+AdditionalParams;
 }
 
 function ChangeFn(DocId){
-if(DocId=="")return
-var answer=window.showModalDialog("ChangeGoods.csp?DocId="+DocId,"","center:yes;status:no;dialogHeight:550px;dialogWidth:800px;resizable:yes;");
+	if(DocId=="")return
+	var answer=window.showModalDialog("ChangeGoods.csp?DocId="+DocId,"","center:yes;status:no;dialogHeight:550px;dialogWidth:800px;resizable:yes;");
 }
 
-
-
-
-	$bind(window,"load",function(){
-		var fr=function(o){ //ищем строку Find Row
-			var tr=null;
-			for (;o.tagName!=="TABLE";o=o.parentElement){
-				if (o.parentElement.tagName==="THEAD") break;
-				if (o.tagName==="TR"){ tr=o; break; }
-			}
-			return tr;
+$bind(window,"load",function(){
+	load(); //основная функция инициализации страницы
+	var fr=function(o){ //ищем строку Find Row
+		var tr=null;
+		for (;o.tagName!=="TABLE";o=o.parentElement){
+			if (o.parentElement.tagName==="THEAD") break;
+			if (o.tagName==="TR"){ tr=o; break; }
 		}
-		$bind($g("tblInstructions"),"mouseover",function(e){
-			if (!e) e=window.event; var tr=fr(e.srcElement); if (tr) tr.className="hover";
-		});
-		$bind($g("tblInstructions"),"mouseout",function(e){
-			if (!e) e=window.event; var tr=fr(e.srcElement); if (tr) tr.className="";
-		});
+		return tr;
+	}
+	$bind($g("TIB"),"mouseover",function(e){
+		if (!e) e=window.event; var tr=fr(e.srcElement); if (tr) tr.className="hover";
 	});
+	$bind($g("TIB"),"mouseout",function(e){
+		if (!e) e=window.event; var tr=fr(e.srcElement); if (tr) tr.className="";
+	});
+});
