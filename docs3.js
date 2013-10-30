06e06ac00cf0332ce8133cd5c0d77fb69574cc74
artemreva79 <artemreva79@gmail.com>
2013-10-30 15:11:41 +0000

    doublefint: Проверка серийных (или гарантийных ) сразу с документа

---
+++ docs3.js
@@ -0,0 +1,24 @@
+// required jquery-1.9.1.js, jquery-ui-1.10.3.custom.js
+$(function(){
+    
+    $('#bCheckSerial').on('click', function(){
+    // кнопка будет видна тогда и только тогда
+    // когда выбран тип документа = ОТЧЕТ О РЕМОНТЕ
+    // на это и рассчитываем.
+    
+    var docid ;  // идентификатор выбранного документа 
+    if (( window.docums ) && ( window.docums.ItemID)){
+        docid = window.docums.ItemID;    
+    }
+    if ( !docid ) {
+        alert( 'Не выбран документ!' );     
+        return;
+    };
+    
+    var url = $( this ).data( 'url' ) + "id="+docid;
+    window.open( url );
+    
+    
+            
+    });
+})    
